c94f6538d2a88fbc11c0f8be1d3d822cd6c5e8b9
yurapero <y1009321@gmail.com>
2023-01-31 13:47:20 +0000

    [Add] homesとitems

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+    
+  namespace :public do
+    root to: "homes#top"
+    get 'homes/about'
+    resources :items, only: [:index, :show]
+  end
+  
   devise_for :admins
   devise_for :costomers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
