fed22de53de05894a05cd496000c8cce1fcb0412
yurapero <y1009321@gmail.com>
2023-02-01 13:39:03 +0000

    [Add] homesのviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
     
+  namespace :admin do
+    resources :sessions, only: [:new, :create, :destroy]
+  end
+  
   namespace :public do
     root to: "homes#top"
     get 'homes/about'
