0ca3a568a81e9d0912d94b3824d55f0c5b7ac886
yurapero <y1009321@gmail.com>
2023-02-07 14:21:18 +0000

    [Add] admin/items

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -24,6 +24,6 @@ class Admin::ItemsController < ApplicationController
   private
   
   def item_params
-    params.require(:item).permit(:image, :name, :introduction, :price)
+    params.require(:item).permit(:image, :name, :introduction, :price, :genre_id, :is_active)
   end
 end
