f22ad0449d310256203327d8566939b085e9cfe2
yurapero <y1009321@gmail.com>
2023-02-12 14:13:04 +0000

    [Add] genres

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -6,15 +6,29 @@ class Admin::GenresController < ApplicationController
   end
 
   def create
-    genre = Genre.new(genre_params)
-    genre.save
-    redirect_to "admin/genres#index"
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to admin_genres_path
+    else
+      @genres = genre.all
+      render :index
+    end
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    genre = Genre.find(genre_params[:id])
+    genre.update(genre_params)
+    redirect_to admin_genres_path
+  end
+  
+  private
+  
+  def genre_params
+    params.require(:genre).permit(:name)
   end
   
 end
