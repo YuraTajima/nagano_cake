ee14c0f0bb3030cf31b63f49f10e4e9f9f395830
yurapero <y1009321@gmail.com>
2023-02-16 14:28:43 +0000

    [Add] customer

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ Customers.create!(
+     email: 'admin@test.com',
+     password: '111111',
+     admin: true
+     )
